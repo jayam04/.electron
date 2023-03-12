307f26fc1d5fad805a9ed200db34bf5be55d9e6d
Jayam Patel <jayam04@pop-os.localdomain>
2023-03-12 12:50:21 +0530

    added individual scripts for apps

---
+++ setup/app_installs/deb_packages/chrome.sh
@@ -0,0 +1,6 @@
+sudo apt install wget
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+rm ./google-chrome-stable_current_amd64.deb
