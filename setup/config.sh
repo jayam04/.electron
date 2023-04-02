73ba63d70d5a85dbc8f95553c5c20619614b988a
jayam04 <93824505+jayam04@users.noreply.github.com>
2023-04-02 20:19:44 +0530

    better structured filed

--- setup/config.sh
+++ setup/config.sh
@@ -1,6 +1,6 @@
 # passwordstore dir
-export PASSWORD_STORE_DIR=$ELECTRON!/passwordstore
+export PASSWORD_STORE_DIR=$ELECTRON/passwordstore
 
 
 # codehub dir
-export CODEHUB_DIR=~/CodeHub
+export CODEHUB_DIR=~/codehub
