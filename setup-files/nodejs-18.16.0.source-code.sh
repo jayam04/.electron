f8c58f6cfe55a62ac0cbedb6e088511aadf216c1
jayam04 <93824505+jayam04@users.noreply.github.com>
2023-07-21 11:22:22 +0530

    added verification and todo to scripts

--- setup-files/nodejs-18.16.0.source-code.sh
+++ setup-files/nodejs-18.16.0.source-code.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cd $APPLICATIONS_FOLDER
+cd /tmp
 
 # Download and extract Node.js stable version
 echo "Downloading Package ..."
-curl -O https://nodejs.org/dist/latest-v18.x/node-v18.16.0-linux-x64.tar.xz
+curl -o node-v18.17.0.tar.gz https://nodejs.org/dist/v18.17.0/node-v18.17.0-linux-x64.tar.xz
 
 # Rename the extracted directory to a more convenient name
 echo "Extracting Package ..."
-tar -xf node-v18.16.0-linux-x64.tar.xz
-rm node-v18.16.0-linux-x64.tar.xz
-mv node-v18.16.0-linux-x64 node-v18.16.0
+tar -xf node-v18.17.0.tar.gz
+rm node-v18.17.0.tar.gz
+mv node-v18.17.0 $APPLICATIONS_FOLDER/node-v18.17.0
 
 # Add Node.js to the system PATH
 echo "Adding Node.js to PATH ..."
-echo '# Node.js 18.16.0' >> $ELECTRON_CONFIGRC
-echo "export PATH=\$PATH:$APPLICATIONS_FOLDER/node-v18.16.0/bin" >> $ELECTRON_CONFIGRC
+echo '# Node.js 18.17.0' >> $ELECTRON_CONFIGRC
+echo "export PATH=\$PATH:$APPLICATIONS_FOLDER/node-v18.17.0/bin" >> $ELECTRON_CONFIGRC
 
 echo "Finished Node.js-18 Setup"
 
